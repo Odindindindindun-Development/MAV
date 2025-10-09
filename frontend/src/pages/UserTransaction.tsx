d633cbac63cd7b5b3c9283a6f186e39ff6ec61bf
unknown <a.tigbabao.550106@umindanao.edu.ph>
2025-10-10 03:40:29 +0800

    changed

--- frontend/src/pages/UserTransaction.tsx
+++ frontend/src/pages/UserTransaction.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Sidebar from "../components/sidebar";
+import Sidebar from "../components/Sidebar";
 import Header from "../components/Header";
 
 const UserTransaction: React.FC = () => {
